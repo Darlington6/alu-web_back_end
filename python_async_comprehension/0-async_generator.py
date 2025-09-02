e4651a43c9dbd129de5e5ea71aee73eb6b9fe72f
Dalington6 <d.tunyinko@alustudent.com>
2025-09-03 01:59:30 +0200

    Async Generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+""" Async Generator """
+
+from asyncio import sleep
+from random import uniform
+from typing import AsyncGenerator, Generator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """ Async Generator """
+    for _ in range(10):
+        await sleep(1)
+        yield uniform(0, 10)
+        
